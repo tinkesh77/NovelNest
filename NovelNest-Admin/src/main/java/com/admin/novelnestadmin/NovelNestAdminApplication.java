502be93999daa0e394f219a876dc876d5a751ab5
adpal2003 <paladarsh601@gmail.com>
2024-08-28 15:56:31 +0530

    Working in user module

--- NovelNest-Admin/src/main/java/com/admin/novelnestadmin/NovelNestAdminApplication.java
+++ NovelNest-Admin/src/main/java/com/admin/novelnestadmin/NovelNestAdminApplication.java
@@ -2,12 +2,13 @@ package com.admin.novelnestadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.admin.novelnestadmin.entity")
 public class NovelNestAdminApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NovelNestAdminApplication.class, args);
     }
-
 }
