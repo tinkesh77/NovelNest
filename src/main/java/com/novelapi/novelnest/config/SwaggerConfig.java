3e4613a9113ad536432b618c4cf7ae500dbe5099
Tinkesh <tinkesh509@gmail.com>
2024-08-29 18:03:33 +0530

    minor chages

--- src/main/java/com/novelapi/novelnest/config/SwaggerConfig.java
+++ src/main/java/com/novelapi/novelnest/config/SwaggerConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+
+    // hello i am here
     @Bean
     public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
