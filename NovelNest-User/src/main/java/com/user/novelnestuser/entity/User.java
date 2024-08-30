cc1dbe58b62739217818c9d0bd675cabf69b747c
adpal2003 <paladarsh601@gmail.com>
2024-08-30 15:41:30 +0530

    make a signup and signin module

---
+++ NovelNest-User/src/main/java/com/user/novelnestuser/entity/User.java
@@ -0,0 +1,45 @@
+package com.user.novelnestuser.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    // Constructor without the 'id' field
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
