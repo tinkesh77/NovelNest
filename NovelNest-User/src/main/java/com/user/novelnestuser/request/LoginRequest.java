61851ccab33820b11535d8f1880b9d3e931bdffd
adpal2003 <paladarsh601@gmail.com>
2024-09-03 19:31:10 +0530

    change

--- NovelNest-User/src/main/java/com/user/novelnestuser/request/LoginRequest.java
+++ NovelNest-User/src/main/java/com/user/novelnestuser/request/LoginRequest.java
@@ -1,24 +1,14 @@
 package com.user.novelnestuser.request;
 
-public class LoginRequest {
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class  LoginRequest {
     private String username;
     private String password;
 
     // Getters and Setters
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/com/novelapi/novelnest/NovelnestApplication.java
+++ src/main/java/com/novelapi/novelnest/NovelnestApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-;
+
 @EntityScan(basePackages = "com.novelapi.novelnest.entity")
 @EnableJpaRepositories(basePackages = "com.admin.novelnestadmin.repositories")
 @EnableWebMvc
